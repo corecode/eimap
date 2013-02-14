963d0b59664512fb03afeeb7e4b5fb8827c1110a
Simon Schubert <2@0x2c.org>
2013-02-14 05:49:42 +0100

    dispatch: pass method for default handler

--- eimap-dispatch.el
+++ eimap-dispatch.el
@@ -15,16 +15,18 @@
 ARGS should be (upcall-data data)."
   (declare (indent 3)
            (doc-string 4)
-           (debug 3))
+           (debug (name defun )))
   `(puthash ',method (lambda ,args ,docstring . ,body) ,(eimap-method-table-name table)))
 
 (defmacro eimap-create-dispatch (table)
   "Returns a lambda suitable for passing to `eimap-open''s `upcall'."
   (let ((table (eimap-method-table-name table)))
     `(lambda (upcall-data method data)
-       (let ((handler (or (gethash method ,table)
-                          (gethash 'default ,table))))
-         (when handler
-             (funcall handler upcall-data data))))))
+       (let ((handler (gethash method ,table)))
+         (if handler
+             (funcall handler upcall-data data)
+           (let ((handler (gethash 'default ,table)))
+             (when handler
+               (funcall handler upcall-data method data))))))))
 
 (provide 'eimap-dispatch)
